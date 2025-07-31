6fbde30cba89cd75b68007a5bdc27f15c6924276
Carlos Batista <carloscoffeepy@gmail.com>
2025-07-31 11:08:30 -0300

    Add infra and utils libraries with exports for error handling and helper functions

---
+++ lib/infra.dart
@@ -0,0 +1,6 @@
+library;
+
+// Infra
+export 'src/infra/errors/default_errors.dart';
+export 'src/infra/errors/try_this.dart';
+export 'src/infra/models/response_model.dart';
---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+library;
+
+// Others
+export 'src/others/ternary_clean.dart';
+
+// Helpers
+export 'src/helpers/datetime_helper.dart';
